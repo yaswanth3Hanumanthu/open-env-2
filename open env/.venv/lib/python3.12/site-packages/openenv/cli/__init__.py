a0dd5fc8c28c987731a1f279a064760b7af35814
yaswanth3Hanumanthu <148070243+yaswanth3Hanumanthu@users.noreply.github.com>
2026-04-09 08:38:57 +0000

    Move OpenEnv project to repo root and finalize submission

--- open env/.venv/lib/python3.12/site-packages/openenv/cli/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-"""OpenEnv CLI package."""
-
-__version__ = "0.1.0"
